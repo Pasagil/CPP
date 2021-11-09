fa354a963c97d1152aa3583d7d0c3b4409b0e31a
Agil <antoine.gilliet@gmail.com>
2021-11-09 15:53:03 +0100

    maj

---
+++ 0311/Cours/test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main(){
+    int p = 3;
+    int* a = &p;
+    std::cout<<a<<std::endl;
+    return 0;
+}
---
+++ 0311/Cours/test_char.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main() {
+    const char* chaine = "Bonjour";
+    std::cout<<chaine<<std::endl;
+    return 0;
+}
